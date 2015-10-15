d671b5a9ffd354824f61f53a7e45f09e1166091c
valladolidita <kelly@Kellys-MacBook-Pro.local>
2015-10-15 15:17:10 -0500

    Exercise 8.2
    MCAD Javascript: Week 8

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+document.getElementsByClassName("mainHeader")
+document.getElementsByClassName("mainParagraph")
+*/
+
+// to change index.html's mainHeader copy in Javascript
+document.getElementsByClassName("mainHeader")[0].innerHTML = "Birds, birds, and more birds";
+
+// to change index.html's mainParagraph copy in Javascript
+document.getElementsByClassName("mainParagraph")[0].innerHTML = "Home to 1,879 species of bird, Colombia has most diversity of birds in the world.";
